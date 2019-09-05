47e6204f173593fed5be7521086b6c2c2d609d22
Yang Shu <963298491@qq.com>
2019-09-05 13:04:05 +0800

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+## the C++ implemententation of LFFD with ncnn 
+  I have implemented the [LFFD](https://arxiv.org/abs/1904.10633)  referring to the official python implementation
+  the Inference time of LFFD with the input shape of 320x240 is 20ms on the Qualcomm Snapdragon 632 CPU
+  
+## TODO(you can refer this implementation to do more)
+ * MNN demo
+ * TensorRT demo: mmxnet model --> onnx-->trt engine
+ * openvino demo: mxnet model-->onnx-->openvino
+  
+  
---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/opencv.hpp>
+#include "LFFD.h"
+#include <chrono>
+
+using namespace cv;
+
+int main(int argc, char** argv) {
+
+	if (argc !=3)
+	{
+		std::cout << " .exe mode_path image_file" << std::endl;
+		return -1;
+	}
+
+	std::string model_path = argv[1];
+	std::string image_file = argv[2];
+
+	LFFD lffd(model_path);
+	cv::Mat frame = cv::imread(image_file);
+	std::vector<FaceInfo> face_info;
+
+	ncnn::Mat inmat = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR, frame.cols, frame.rows);
+	lffd.detect(inmat, face_info, 320, 240);
+
+	for (int i = 0; i < face_info.size(); i++)
+	{
+		auto face = face_info[i];
+		cv::Point pt1(face.x1, face.y1);
+		cv::Point pt2(face.x2, face.y2);
+		cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
+	}
+
+	cv::namedWindow("lffd", CV_WINDOW_NORMAL);
+	cv::imshow("lffd", frame);
+	cv::waitKey();
+	return 0;
+}
